32db81c08c6e7a53ea03064a5977aa901f3b1618
Lucas Souza <lhenriquesouza00@gmail.com>
2021-03-16 21:43:39 -0300

    API REST com Spring Boot - parte 2: Configurando banco de dados Postgres e criando Model

---
+++ src/main/java/br/com/produtos/apirest/repository/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package br.com.produtos.apirest.repository;
+
+import br.com.produtos.apirest.models.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
